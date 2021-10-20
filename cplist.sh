40efa4980a9d560fb755457110718a07ef1a7f95
Yongmin Hong <revi@pobox.com>
2021-10-21 06:23:24 +0900

    Use Virtualenv
    Summary:
    Ref T312. Part of update.
    
    # Signed-off-by: Yongmin Hong <revi@pobox.com>
    
    Test Plan: There is No Such Testing :tm:
    
    Reviewers: #acl_sudoers, #code_meister, revi
    
    Reviewed By: #acl_sudoers, revi
    
    Maniphest Tasks: T312
    
    Differential Revision: https://bugs.revi.xyz/D15

--- cplist.sh
+++ cplist.sh
@@ -4,4 +4,4 @@
 # Login is handled by BotPassword.
 
 cd revibot # Go to pywikibot core
-python3 ../core/pwb.py community_portal_list # Actual scripts - clean the sandbox.
+$HOME/core/bin/python3 ../core/pwb.py community_portal_list # Actual scripts - update the Community Portal list.
